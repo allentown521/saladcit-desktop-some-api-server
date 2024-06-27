d3909904672592770a44cf924e106c7bb5fc5bfa
Pylogmon <pylogmon@outlook.com>
2024-06-27 19:42:10 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ pub static AFDIAN_USER_ID: &str = "AFDIAN_USER_ID";
 pub static AFDIAN_TOKEN: &str = "AFDIAN_TOKEN";
 pub static ALIPAN_CLIENTID: &str = "ALIPAN_CLIENTID";
 pub static ALIPAN_SECRET: &str = "ALIPAN_SECRET";
+pub static PORT: u32 = 8001;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -17,7 +18,7 @@ async fn main() -> std::io::Result<()> {
             .service(ali_qrcode)
             .service(ali_access_token)
     })
-    .bind(("127.0.0.1", 8001))?
+    .bind(("127.0.0.1", PORT))?
     .run()
     .await
 }
